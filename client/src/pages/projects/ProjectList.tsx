81b979881278be65df58a0d9192e0dddb2caec09
James <jamesrobertsonmcfc@gmail.com>
2026-09-09 12:59:38 +0100

    fixed button redirect

--- client/src/pages/projects/ProjectList.tsx
+++ client/src/pages/projects/ProjectList.tsx
@@ -147,23 +147,25 @@ function ProjectList() {
                       Create your first project to start organising your work.
                     </p>
 
-                    <button type="button" className={`mt-5 ${buttonStyles}`}>
-                      <svg
-                        className="h-5 w-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={2}
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                      </svg>
-                      Create your first project
-                    </button>
+                    <Link to={`/projects/create`}>
+                      <button type="button" className={`mt-5 ${buttonStyles}`}>
+                        <svg
+                          className="h-5 w-5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={2}
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                          />
+                        </svg>
+                        Create your first project
+                      </button>
+                    </Link>
                   </div>
                 )
               ) : (
